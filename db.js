a496211d576e2f9abbba9e3c427a060ca0aca6f0
Felipe Miguel <felipemiguel.dev@gmail.com>
2026-06-14 12:23:59 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production"
+        ? {
+            rejectUnauthorized: false
+        }
+        : false
+});
+
+pool.on("error", (err) => {
+    console.error("Erro PostgreSQL:", err);
 });
 
 module.exports = pool;
